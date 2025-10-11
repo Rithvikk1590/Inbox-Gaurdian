eee6da9096b2e7dbdb26ebe5b4ef5730255ed6b1
kohbgk <your_email@example.com>
2025-10-11 09:31:04 +0800

    bug fix

--- Website/email_analyser/url_analyser.py
+++ Website/email_analyser/url_analyser.py
@@ -1,5 +1,6 @@
 import re
 from config.url_shorteners import URL_SHORTENERS as url_shorteners
+from urllib.parse import urlparse
 
 def analyse_urls(email_data):
     body = email_data.get("body", "")
@@ -15,6 +16,7 @@ def analyse_urls(email_data):
         url_lower = url.lower()
         print(url_lower)
         detected_risks = []
+        print("shortener", url_lower)
 
         # IP address detection
         if re.match(r'https?://(\d{1,3}\.){3}\d{1,3}', url_lower):
@@ -26,13 +28,17 @@ def analyse_urls(email_data):
                 "risk_level": "high"
             })
             continue
-
+        
         # URL shortener detection (unchanged logic, now reads from separate file)
         if any(shortener in url_lower for shortener in url_shorteners):
             risk += 8
             detected_risks.append(f"Shortened URL detected. Risk +{risk}.")
+            # Extract only hostname from the URL
+            parsed_url = urlparse(url)
+            domain = parsed_url.netloc or url
+            
             highlights.append({
-                "text": url,
+                "text": domain,
                 "hover_message": " ".join(detected_risks),
                 "risk_level": "high"
             })
